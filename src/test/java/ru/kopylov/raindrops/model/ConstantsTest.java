8a02faf628161963484e3a7b73d37dfc889e8cfa
sv-kopylov <imber81@gmail.com>
2019-08-27 08:06:22 +0300

    estimate irl contants

--- src/test/java/ru/kopylov/raindrops/model/ConstantsTest.java
+++ src/test/java/ru/kopylov/raindrops/model/ConstantsTest.java
@@ -36,4 +36,27 @@ public class ConstantsTest {
         };
         System.out.println(cstns.SpaceLenght());
     }
+
+    @Test
+    public void probability(){
+        double intensity = 5.0/360.0;
+        double dropDiameter = 1.0; // mm
+        double r = (dropDiameter/2.0)*(1.0/100.0);
+        double dropVolume = (4.0/3.0)*Math.PI*r*r*r;
+        double dropSpeed = DropFallingSpeed(dropDiameter); //1775 см/сек
+        double timePerLayer = 1.0/dropSpeed;
+        double waterPer1m2 = intensity*timePerLayer;
+        double dropsIn1m2 = waterPer1m2/dropVolume;
+        System.out.printf("intensity: %.8f liter per second\n", intensity);
+        System.out.printf("drop volume: %.8f  liters\n",dropVolume);
+        System.out.printf("drop diameter: %.8f  milimeter\n",dropDiameter);
+        System.out.printf("drop speed: %.8f cm per sec\n", dropSpeed);
+        System.out.printf("time per layer: %.8f sec\n", timePerLayer);
+        System.out.printf("water in one  layer: %.8f liters \n", waterPer1m2);
+        System.out.printf("drops in one  layer: %.8f \n", dropsIn1m2);
+
+
+
+
+    }
 }
