ad24e1753c2cc46e6809e190830607155dd4a41c
sv-kopylov <imber81@gmail.com>
2019-10-07 20:15:10 +0300

    added rain space visualisation 3d

--- src/main/java/ru/kopylov/raindrops/application/Application.java
+++ src/main/java/ru/kopylov/raindrops/application/Application.java
@@ -15,22 +15,36 @@ import ru.kopylov.raindrops.persist.ResultsDAO;
 public class Application {
     public static Logger logger = Logger.getLogger(Application.class);
 
+    public static int LOW_HUMAN_SPEED  = 61; // прогулочный шаг
+    public static int MIDDLE_HUMAN_SPEED  = 640; // 23 км/ч довольно энергичный бег
+    public static int HI_HUMAN_SPEED  = 1219; // рекорд скорости
+
     public void launch(){
-        logger.debug("Start launch");
 
         RainSpace rainSpace = new RainSpace();
         Human human = new Human();
         InputDataSet dataSet = InputDataSet.getInstance();
 
         DataSource dataSource = new DataSource();
+
         InputDataSetDAO inputDataSetDAO = new InputDataSetDAO(dataSource);
         ResultsDAO resultsDAO = new ResultsDAO(dataSource);
 
+
+
+/********************************   1 сет экспериментов    **************************************
+ * 1)	Интенсивность:  При постоянном размере капли проводится несколько сетов с различной интенсивностью.
+ * Цель – нужно ли менять стратегию при разной интенсивности.
+ */
+
 //        Заполняем пространство дождем
-        logger.debug("Start space filling by rain");
-        for(int i=0;i<dataSet.getSpaceHeight();i++){
-            rainSpace.updateTopLayer();
-        }
+        fillSpaceByRain(rainSpace);
+
+        dataSet.setHumanSpeed(LOW_HUMAN_SPEED);
+        runDistance(rainSpace, human, inputDataSetDAO, resultsDAO);
+        human.reset();
+
+
 
 //        начало эксперимента
         dataSet.setHumanSpeed(70);
@@ -45,6 +59,14 @@ public class Application {
         resultsDAO.shutDown();
         dataSource.closeConnection();
 
+    }
+
+    void fillSpaceByRain(RainSpace rainSpace){
+        //        Заполняем пространство дождем
+        logger.debug("Start space filling by rain");
+        for(int i=0;i<InputDataSet.getInstance().getSpaceHeight();i++){
+            rainSpace.updateTopLayer();
+        }
     }
 
     void runDistance(RainSpace rainSpace,  Human human, InputDataSetDAO inputDataSetDAO, ResultsDAO resultsDAO){
